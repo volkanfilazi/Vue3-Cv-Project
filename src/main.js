b5d03e515101e4461e93cd29789c0d31b90f447b
volkan filazi <74497449+volkanfilazi@users.noreply.github.com>
2023-06-02 22:09:42 +0200

    intro has been updated

--- src/main.js
+++ src/main.js
@@ -4,9 +4,8 @@ import './style.css'
 import App from './App.vue'
 import Particles from "vue3-particles";
 import { MotionPlugin } from '@vueuse/motion'
-import VolkiButton from 'volki-components'
 
 
 const pinia = createPinia()
 
-createApp(App).use(VolkiButton).use(Particles).use(pinia).use(MotionPlugin).mount('#app')
+createApp(App).use(Particles).use(pinia).use(MotionPlugin).mount('#app')
