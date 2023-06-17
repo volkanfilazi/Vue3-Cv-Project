3f67952d3d029d9cfc0b6ae643e71c00cf9b1c57
volkan filazi <volkanfilazi@hotmail.com>
2023-06-17 17:36:34 +0200

    changed shopproject folder design

---
+++ src/restaurantComponents/ProductList.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import { useStorage } from '@vueuse/core';
+import { useShopStore } from '../Store/Shop';
+import router from '../router/router';
+const shopStore = useShopStore()
+const selectedProductId = useStorage("id", Number)
+import { Icon } from '@iconify/vue';
+
+
+function getId(id: number) {
+  selectedProductId.value = undefined
+  selectedProductId.value = id
+  router.push({ name: 'shopDetail', params: { id: selectedProductId.value } })
+}
+
+async function addCartAndSendtoBasket(productId: number, title: string, price: string, count: string, image: string) {
+  const response = shopStore.addToCart(productId, title, price, count, image)
+  shopStore.getCarts()
+  router.push({ name: 'basket' })
+}
+</script>
+
+<template>
+  <div v-if="shopStore.allProductsClone.length > 0" class="md:w-full mt-5 gap-3 grid grid-cols-1 w-full"
+    :class="{ 'grid lg:grid-cols-3': shopStore.allProductsClone.length > 0 }">
+    <div class="p-2 rounded-md bg-white h-[200px] relative md:mt-3 cursor-pointer flex"
+      v-for="products in shopStore.allProductsClone">
+      <div v-if="products.id > 0 && products.id < 5"
+        class="w-10 h-6 bg-purple-600 absolute -top-3 flex items-center justify-center text-white text-[13px] right-0">
+        <p>New</p>
+      </div>
+      <div v-if="products.id > 11 && products.id < 15"
+        class="w-10 h-6 bg-purple-500 absolute -top-3 flex items-center justify-center text-white text-[13px] right-0">
+        <p>Best</p>
+      </div>
+      <div class="w-1/2 flex gap-5">
+        <img class="p-3" :src="products.image" alt="">
+      </div>
+
+      <div class="w-1/2 flex flex-col h-full">
+        <div
+          class="product-title h-1/2 text-gray-600 flex items-center cursor-pointer transition-all duration-200 hover:text-orange-600">
+          <p @click="getId(products.id)">{{ products.title.substring(0, 20) }}</p>
+        </div>
+        <div class="h-1/2">
+          <p class="text-[18px] font-bold text-gray-700">€ {{ products.price }}</p>
+        </div>
+        <div class="min-w-[160px] h-1/3 flex items-end gap-1 p-1">
+          <button @click="shopStore.addToCart(products.id, products.title, products.price, '0', products.image)"
+            class=" border-[1px] w-1/3 flex items-center justify-center cursor-pointer p-1 border-gray-400 group hover:bg-purple-500 transition-all duration-300 text-blue-500 font-bold">
+            <Icon class="text-gray-500 transition-all duration-300 group-hover:text-white" icon="simple-line-icons:basket"
+              width="24" height="24" />
+          </button>
+          <button @click="addCartAndSendtoBasket(products.id, products.title, products.price, '0', products.image)"
+            class=" border-[1px] w-2/3 cursor-pointer p-1 border-blue-500 hover:bg-purple-500 transition-all duration-300 hover:text-white text-blue-500 font-serif font-bold">
+            buy now</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+@font-face {
+  font-family: 'rabikiso';
+  src: url('../assets/fonts/MerriweatherSans-Bold.ttf') format('truetype');
+}
+
+.product-title {
+  font-family: 'Franklin Gothic Medium',
+}
+
+.product-desc {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
---
+++ src/restaurantComponents/UpperImages.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-full lg:w-5/6 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 bg-orange-500">
+    <div class=" h-[270px] relative text-white items-center flex justify-center gap-5 cursor-pointer">
+      <div class="bg-auto relative lg:bg-no-repeat bg-center h-full w-full"
+        style="background-image: url('https://i.pinimg.com/originals/c9/79/b0/c979b0df5bdb8538117524e7fd4346c1.jpg')">
+        <div class="absolute text-red-800 p-10 space-y-4">
+          <div class="text-2xl">DO YOU BELIEVE MAGIC?</div>
+          <div class="text-2x1">10% OFF</div>
+          <button
+            class="border-[1px] hover:border-purple-600 p-2 transition-all duration-300 hover:text-purple-600">DISCOVER</button>
+        </div>
+      </div>
+    </div>
+    <div class="h-[270px] text-white items-center flex justify-center gap-5">
+      <div class="bg-auto relative lg:bg-no-repeat bg-center h-full w-full"
+        style="background-image: url('https://t3.ftcdn.net/jpg/03/94/11/10/360_F_394111088_TCUFgYfGsRqOVDddr29BOXYd9N4j8yDR.jpg')">
+        <div class="absolute text-red-800 p-10 space-y-4">
+          <div class="text-2xl">MAKE UP YOURSELF</div>
+          <div class="text-2x1">50% OFF</div>
+          <button
+            class="border-[1px] border-red-800 hover:border-purple-600 p-2 transition-all duration-300 hover:text-purple-600">DISCOVER</button>
+        </div>
+      </div>
+    </div>
+    <div class="h-[270px] text-white items-center flex justify-center gap-5 group">
+      <div class="bg-auto relative lg:bg-no-repeat bg-center h-full w-full"
+        style="background-image: url('https://img.freepik.com/premium-photo/ring-with-diamonds-black-background-3d-rendering_34478-2539.jpg')">
+        <div class="absolute text-white p-10 space-y-4">
+          <div class="text-2xl">NATURAL ITEMS</div>
+          <div class="text-2x1">15% OFF</div>
+          <button
+            class="border-[1px] z-10 hover:border-purple-600 p-2 transition-all duration-300 hover:text-purple-600">DISCOVER</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
