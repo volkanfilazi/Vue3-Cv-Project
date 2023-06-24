f500a19c5f56bad4af4f97a1424fa17d133514cd
volkan filazi <volkanfilazi@hotmail.com>
2023-06-24 19:27:36 +0200

    resume component updated

--- src/pages/SecondCv/SecondCvMain.vue
+++ src/pages/SecondCv/SecondCvMain.vue
@@ -1,115 +1,53 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useIntroSocialMedia } from '../../Store/IntroSocialMedia.store';
 import Intro from '../../secondCv/Intro.vue'
 import Tools from '../../secondCv/Tools.vue'
 import Languages from '../../secondCv/Languages.vue';
 import Specialization from '../../secondCv/Specialization.vue'
+import Resume from '../../secondCv/Resume.vue'
+import { useSliderStore } from '../../Store/SecondCv/SliderToogles.store';
 
-const IntroSocialMedia = useIntroSocialMedia()
-
-const intro = ref(true)
-const tools = ref(false)
-const languages = ref(false)
-const specialization = ref(false)
-const resume = ref(false)
-
-function introTrigger(){
-  tools.value = false
-  languages.value = false
-  specialization.value = false
-  resume.value = false
-}
-
-function toolsTrigger(){
-  tools.value = true
-  languages.value = false
-  specialization.value = false
-  resume.value = false
-}
-
-function languagesTrigger(){
-  languages.value = true
-  tools.value = false
-  specialization.value = false
-  resume.value = false
-}
-
-function specializationTrigger(){
-  specialization.value = true
-  languages.value = false
-  tools.value = false
-  resume.value = false
-}
-
-function resumeTrigger(){
-  resume.value = true
-  specialization.value = false
-  languages.value = false
-  tools.value = false
-}
-
+const sliderStore = useSliderStore()
 </script>
 
 <template>
   <div class="h-4/4 w-full">
-
     <div class="bg-black hidden md:grid grid-cols-5 items-center justify-center w-full h-1/4">
-      <button @click="introTrigger()" class="bg-red-800 p-2 h-full text-white">Intro</button>
+      <button @click="sliderStore.introTrigger()" class="bg-red-800 p-2 h-full text-white">Intro</button>
 
-      <button @click="toolsTrigger()" class="bg-purple-900 p-2 h-full text-white">Tools</button>
+      <button @click="sliderStore.toolsTrigger()" class="bg-purple-900 p-2 h-full text-white">Tools</button>
 
-      <button @click="languagesTrigger()" class="bg-lime-800 p-2 h-full text-white">Languages</button>
+      <button @click="sliderStore.languagesTrigger()" class="bg-lime-800 p-2 h-full text-white">Languages</button>
 
-      <button @click="specializationTrigger()" class="bg-orange-800 p-2 h-full text-white">Specialization</button>
+      <button @click="sliderStore.specializationTrigger()" class="bg-orange-800 p-2 h-full text-white">Specialization</button>
 
-      <button @click="resumeTrigger()" class="bg-gray-700 p-2 h-full text-white">Resume</button>
+      <button @click="sliderStore.resumeTrigger()" class="bg-gray-700 p-2 h-full text-white">Resume</button>
     </div>
 
     <div class="flex flex-col md:flex-row w-full h-3/4">
 
       <div class="md:translate-x-full md:opacity-0 md:fixed transition-all duration-300 w-full h-full bg-red-800 md:bg-red-800"
-        :class="[{ 'md:!translate-x-0': intro === true }, { 'md:!opacity-100': intro === true }]">
+        :class="[{ 'md:!translate-x-0': sliderStore.intro === true }, { 'md:!opacity-100': sliderStore.intro === true }]">
         <Intro></Intro>
       </div>
 
       <div class="md:translate-x-full md:opacity-0 md:fixed transition-all duration-500 w-full h-full bg-red-800 md:bg-purple-900"
-        :class="[{ 'md:!translate-x-0': tools }, { 'md:!opacity-100': tools }]">
+        :class="[{ 'md:!translate-x-0': sliderStore.tools }, { 'md:!opacity-100': sliderStore.tools }]">
         <Tools></Tools>
       </div>
 
       <div class="md:translate-x-full md:opacity-0 md:fixed transition-all duration-500 w-full h-full bg-red-800 md:bg-green-950"
-        :class="[{ 'md:!translate-x-0': languages }, { 'md:!opacity-100': languages }]">
+        :class="[{ 'md:!translate-x-0': sliderStore.languages }, { 'md:!opacity-100': sliderStore.languages }]">
         <Languages></Languages>
       </div>
 
       <div class="md:translate-x-full md:opacity-0 md:fixed transition-all duration-500 w-full h-full bg-red-800 md:bg-orange-800"
-        :class="[{ 'md:!translate-x-0': specialization }, { 'md:!opacity-100': specialization }]">
+        :class="[{ 'md:!translate-x-0': sliderStore.specialization }, { 'md:!opacity-100': sliderStore.specialization }]">
         <Specialization></Specialization>
       </div>
 
       <div class="md:translate-x-full md:opacity-0 md:fixed transition-all duration-500 w-full h-full bg-red-800 md:bg-gray-900"
-        :class="[{ 'md:!translate-x-0': resume }, { 'md:!opacity-100': resume }]">
-        <div class="flex flex-col h-full md:flex-row w-3/3 md:h-full">
-          <div class="flex items-center w-full md:w-1/4">
-            <h1 class="text-white w-full text-center p-5 md:-rotate-90 font-bold tracking-widest text-3xl md:text-5xl">
-              ABOUT
-              ME</h1>
-          </div>
-          <div class="md:w-3/4 flex flex-col gap-5 items-center justify-center text-white">
-            <h1 class="text-2xl md:text-4xl font-bold text-white">{{ $t('myname') }}<span class="text-yellow-400"> VOLKAN
-                FILAZI</span></h1>
-            <h2 class="text-sm md:text-2xl text-center sm:text-center md:text-start font-bold text-white">{{ $t('iamaweb')
-            }}
-            </h2>
-            <div class="flex flex-col gap-5 mt-5 md:justify-center md:flex-row">
-              <p class="text-yellow-400">+10 <span class="text-white">asdasd</span></p>
-              <p class="text-yellow-400">+2 <span class="text-white">certificate</span></p>
-              <p class="text-yellow-400">+3 <span class="text-white">years experience</span></p>
-              <p class="text-yellow-400">+3 <span class="text-white">program languages</span></p>
-            </div>
-          </div>
-        </div>
+        :class="[{ 'md:!translate-x-0': sliderStore.resume }, { 'md:!opacity-100': sliderStore.resume }]">
+        <Resume></Resume>
       </div>
     </div>
 
@@ -119,6 +57,5 @@ function resumeTrigger(){
 <style scoped>
 :disabled {
   opacity: 0.1;
-  position: ;
 }
 </style>
