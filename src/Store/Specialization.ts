6fc9aef55a4a12eea17264228a723ecbe8f48c80
volkan filazi <volkanfilazi@hotmail.com>
2023-06-28 23:11:17 +0200

    added advanced filter(program languages)

--- src/Store/Specialization.ts
+++ src/Store/Specialization.ts
@@ -4,7 +4,7 @@ import { ref } from 'vue'
 export const useSpecializationStore = defineStore('specialization', () => {
   
     const shopApp = ref(['Vue.js','HTML','CSS','Tailwind','Typescript','Axios'])
-    
+    const copySpecializationArray = ref<any[]>([])
     const specializationArray = [
       {
         status: 'Online',
@@ -13,7 +13,7 @@ export const useSpecializationStore = defineStore('specialization', () => {
         name: 'Shop',
         image: 'guidance:shop',
         thema: 'Shop, Order, Filter System, Checkout, Networking',
-        frameworks: 'Vue.js, HTML, CSS, Tailwind, Typescript',
+        frameworks: ['Vue.js', 'HTML', 'CSS', 'Tailwind', 'Typescript'],
         packages: '@iconify/vue, axios'
       },
       {
@@ -23,28 +23,8 @@ export const useSpecializationStore = defineStore('specialization', () => {
         name: 'Intelligence Square',
         image: 'carbon:ibm-watson-knowledge-studio',
         thema: 'Knowledge Competition, Multiple categories, Local Backend',
-        frameworks: ' Vue.js, HTML, CSS, Tailwind',
+        frameworks: ['Vue.js', 'HTML', 'CSS', 'Tailwind'],
         packages: '@iconify/vue'
-      },
-      {
-        status: 'Online',
-        liked: false,
-        numberOfLikes: 20,
-        name: 'Second Cv',
-        image: 'mdi:resume',
-        thema: 'Resume, Tools, Languages, Specialization',
-        frameworks: 'Vue.js, HTML, CSS, Tailwind, Typescript',
-        packages: '@iconify/vue, vue-router, vite'
-      },
-      {
-        status: 'Continues',
-        liked: false,
-        numberOfLikes: 5,
-        name: 'Encryption',
-        image: 'carbon:ibm-cloud-hyper-protect-crypto-services',
-        thema: 'Strengthen Passwords',
-        frameworks: 'Vue.js, HTML, CSS, Tailwind',
-        packages: '@iconify/vue, jwt, decode'
       },
       {
         status: 'Offline',
@@ -53,12 +33,45 @@ export const useSpecializationStore = defineStore('specialization', () => {
         name: 'Desk Booking System',
         image: 'tabler:brand-booking',
         thema: 'Login System, Desk Reservation, Admin Panel, Offices , Desks, Users',
-        frameworks: 'Vue.js, HTML, CSS, Tailwind',
+        frameworks: ['Vue.js', 'HTML', 'CSS', 'Tailwind'],
         packages: '@iconify/vue, jwt, decode, axios, vite, vueuse-usestorage, iconify'
+      },
+      {
+        status: 'Continues',
+        liked: false,
+        numberOfLikes: 5,
+        name: 'Encryption',
+        image: 'carbon:ibm-cloud-hyper-protect-crypto-services',
+        thema: 'Strengthen Passwords',
+        frameworks: ['Vue.js', 'HTML', 'CSS', 'Tailwind'],
+        packages: '@iconify/vue, jwt, decode'
+      },
+      {
+        status: 'Continues',
+        liked: false,
+        numberOfLikes: 20,
+        name: 'Second Cv',
+        image: 'mdi:resume',
+        thema: 'Resume, Tools, Languages, Specialization',
+        frameworks: ['Vue.js', 'HTML', 'CSS', 'Tailwind', 'Typescript'],
+        packages: '@iconify/vue, vue-router, vite'
+      },
+      {
+        status: 'Offline',
+        liked: false,
+        numberOfLikes: 32,
+        name: 'Codagram',
+        image: 'mdi:resume',
+        thema: 'Fake Instagram, Firebase Login System',
+        frameworks: ['Kotlin'],
+        packages: 'retrofit, firebase'
       }
     ]
+
+    copySpecializationArray.value = specializationArray
     return {
       shopApp,
-      specializationArray   
+      specializationArray,
+      copySpecializationArray   
     }
 })
--- src/components/Specialization.vue
+++ src/components/Specialization.vue
@@ -4,12 +4,13 @@ import { useRouter } from "vue-router";
 import VButton from './V-Base/V-Button.vue';
 import { useSpecializationStore } from '../Store/Specialization';
 import { useStorage } from '@vueuse/core';
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
+import ProjectsFilter from './ProjectsFilter.vue';
 
 const specializationStore = useSpecializationStore()
 const darkmode = useStorage("cvDarkmode", Boolean)
 const like = useStorage("like", Array(specializationStore.specializationArray.length).fill(false))
-const likeAnimation = ref(Array(specializationStore.specializationArray.length).fill(Boolean))
+const likeAnimation = ref(Array(specializationStore.specializationArray.length).fill(false))
 const router = useRouter()
 
 function likeUpdate(index) {
@@ -19,6 +20,9 @@ function likeUpdate(index) {
     likeAnimation.value[index] = false
   }, 2000)
 }
+
+const vue = ref('')
+
 
 </script>
 <template>
@@ -32,52 +36,71 @@ function likeUpdate(index) {
     </div>
     <input
       class="outline-none bg-transparent focus:text-yellow-400 focus:border-yellow-400 px-3 border-[1px] rounded-md w-1/2 p-1"
-      placeholder="Search a project name" type="text" :class="[{ '!border-black': darkmode }, { '!text-black': darkmode }]">
-    <div
-      class="w-full md:w-4/5 p-1 md:p-0 text-white text-sm md:text-lg md:mt-5 gap-2 md:h-1/2 grid grid-cols-1 md:grid-cols-3">
-      <div v-for="(items, index) in specializationStore.specializationArray"
-        class="w-full group border-[1px] flex flex-col md:h-[500px] transition-all duration-300 hover:border-yellow-400"
-        :class="[{ 'hover:!border-red-700': darkmode }, { 'border-black': darkmode }]">
+      placeholder="Search a project name" type="text"
+      :class="[{ '!border-black': darkmode }, { '!text-black': darkmode }]">
+    <div class="flex flex-col md:flex-row w-full gap-2">
+      <ProjectsFilter></ProjectsFilter>
+      <div
+        class="w-full lg:w-4/5 p-1 md:p-0 text-white text-sm md:text-lg md:mt-5 gap-2 md:h-1/2 grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+        <div v-for="(items, index) in specializationStore.copySpecializationArray"
+          class="w-full group border-[1px] flex flex-col md:h-[500px] transition-all duration-300 hover:border-yellow-400"
+          :class="[{ 'hover:!border-red-700': darkmode }, { 'border-black': darkmode }]">
 
-        <div class="h-2/5 sm:h-1/3 flex items-center justify-center flex-col gap-5">
-          <div class="flex w-full items-center gap-1 px-2 justify-between">
-            <div class="flex w-full items-center gap-1 px-2">
-              <p :class="{ '!text-black': darkmode }">Hosting : </p>
-              <div class="w-4 h-4 bg-green-500 rounded-full"
-                :class="[{ '!bg-red-500': items.status === 'Offline' }, { '!bg-yellow-500': items.status === 'Continues' }]">
+          <div class="h-2/5 sm:h-1/3 flex items-center justify-center flex-col gap-5">
+            <div class="flex w-full items-center gap-1 px-2 justify-between">
+              <div class="flex w-full items-center gap-1 px-2">
+                <p :class="{ '!text-black': darkmode }">Hosting : </p>
+                <div class="w-4 h-4 bg-green-500 rounded-full"
+                  :class="[{ '!bg-red-500': items.status === 'Offline' }, { '!bg-yellow-500': items.status === 'Continues' }]">
+                </div>
+              </div>
+              <div class="relative">
+                <span class="text-yellow-400 absolute -top-6"
+                  :class="{ 'animate-bounce': likeAnimation[index] && like[index] }">{{ likeAnimation[index] &&
+                    like[index]
+                    ? '+1' : '' }}</span>
+                <p>{{ like[index] ? items.numberOfLikes + 1 : items.numberOfLikes }}</p>
+              </div>
+              <Icon @click="likeUpdate(index)" class="cursor-pointer text-gray-500" icon="icon-park-solid:like" width="30"
+                height="30" :class="{ 'text-yellow-400': like[index] }" />
+            </div>
+            <h1 class="transition-all duration-300 group-hover:text-yellow-400 text-center text-xl"
+              :class="[{ 'group-hover:!text-red-700': darkmode }, { 'text-black': darkmode }]">~ {{ items.name }} ~</h1>
+            <Icon class="transition-all duration-300 group-hover:text-yellow-400"
+              :class="[{ 'group-hover:!text-red-700': darkmode }, { 'text-black': darkmode },]" :icon="items.image"
+              width="36" height="36" />
+          </div>
+
+          <div class="h-2/5 sm:h-2/3 md:justify-center flex flex-col p-2">
+            <p class="text-yellow-400" :style="{ color: darkmode ? 'red' : '' }">Project Thema : <span class="text-white"
+                :style="{ color: darkmode ? 'black' : '' }">{{ items.thema }}</span></p>
+            <p class="text-yellow-400" :style="{ color: darkmode ? 'red' : '' }">Languages and Frameworks :
+            <div class="grid grid-cols-3 gap-2 text-center">
+              <div :style="{ color: darkmode ? 'black' : '' }" v-for="elements in items.frameworks">
+                <p class="text-orange-500 rounded-lg border-[1px]">{{ elements }}</p>
               </div>
             </div>
-            <div class="relative">
-              <span class="text-yellow-400 absolute -top-6" :class="{ 'animate-bounce': likeAnimation[index] && like[index] }">{{ likeAnimation[index] && like[index] ? '+1' : '' }}</span>
-              <p>{{ like[index] ? items.numberOfLikes + 1 : items.numberOfLikes }}</p>
-            </div>
-            <Icon @click="likeUpdate(index)" class="cursor-pointer text-gray-500" icon="icon-park-solid:like" width="30"
-              height="30" :class="{ 'text-yellow-400': like[index] }" />
+            </p>
+            <p class="text-yellow-400" :style="{ color: darkmode ? 'red' : '' }">Used Packages : <span class="text-white"
+                :style="{ color: darkmode ? 'black' : '' }">{{ items.packages }}</span></p>
           </div>
-          <h1 class="transition-all duration-300 group-hover:text-yellow-400 text-center text-xl"
-            :class="[{ 'group-hover:!text-red-700': darkmode }, { 'text-black': darkmode }]">~ {{ items.name }} ~</h1>
-          <Icon class="transition-all duration-300 group-hover:text-yellow-400"
-            :class="[{ 'group-hover:!text-red-700': darkmode }, { 'text-black': darkmode },]" :icon="items.image"
-            width="36" height="36" />
+
+          <div class="flex h-1/5 justify-center mt-5 md:mt-0 items-center md:mb-0">
+            <VButton v-if="items.name === 'Shop'" @click="router.push({ name: 'shop' })">Live Preview</VButton>
+            <VButton v-if="items.name === 'Intelligence Square'" @click="router.push({ name: 'intelligenceSquare' })">Live
+              Preview
+            </VButton>
+            <a href="https://github.com/volkanfilazi/DeskBookingSystem">
+              <VButton v-if="items.name === 'Desk Booking System'">Github</VButton>
+            </a>
+            <VButton v-if="items.name === 'Second Cv'" @click="router.push({ name: 'secondCv' })" disabled>Coming Soon
+            </VButton>
+            <VButton v-if="items.name === 'Encryption'" disabled>Coming Soon</VButton>
+            <VButton v-if="items.name === 'Codagram'" disabled>Coming Soon</VButton>
+          </div>
         </div>
-        <div class="h-2/5 sm:h-2/3 md:justify-center flex flex-col p-2">
-          <p class="text-yellow-400" :style="{ color: darkmode ? 'red' : '' }">Project Thema : <span class="text-white"
-              :style="{ color: darkmode ? 'black' : '' }">{{ items.thema }}</span></p>
-          <p class="text-yellow-400" :style="{ color: darkmode ? 'red' : '' }">Languages and Frameworks : <span
-              class="text-white" :style="{ color: darkmode ? 'black' : '' }">{{ items.frameworks }}</span></p>
-          <p class="text-yellow-400" :style="{ color: darkmode ? 'red' : '' }">Used Packages : <span class="text-white"
-              :style="{ color: darkmode ? 'black' : '' }">{{ items.packages }}</span></p>
-        </div>
-        <div class="flex h-1/5 justify-center items-center md:mb-0">
-          <VButton v-if="items.name === 'Second Cv'" @click="router.push({ name: 'secondCv' })">Live Preview</VButton>
-          <VButton v-if="items.name === 'Shop'" @click="router.push({ name: 'shop' })">Live Preview</VButton>
-          <VButton v-if="items.name === 'Intelligence Square'" @click="router.push({ name: 'intelligenceSquare' })">Live Preview
-          </VButton>
-          <a href="https://github.com/volkanfilazi/DeskBookingSystem">
-            <VButton v-if="items.name === 'Desk Booking System'">Github</VButton>
-          </a>
-          <VButton v-if="items.name === 'Encryption'" disabled>Coming Soon</VButton>
       </div>
     </div>
+
   </div>
-</div></template>
+</template>
