7ac5ecbb1c65c18c5d4616c874b05758940163f8
volkan filazi <volkanfilazi@hotmail.com>
2023-06-29 16:50:39 +0200

    added advenced filter functions

--- src/components/ProjectsFilter.vue
+++ src/components/ProjectsFilter.vue
@@ -1,108 +1,97 @@
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue';
+import { onMounted, ref, watchEffect } from 'vue';
 import { useSpecializationStore } from '../Store/Specialization';
-const vue = ref(false)
-const typescript = ref(false)
-const kotlin = ref(false)
+import { Icon } from '@iconify/vue';
 
 const specializationStore = useSpecializationStore()
 
-function zero() {
-  if (vue.value === true) {
-    specializationStore.copySpecializationArray = specializationStore.specializationArray.filter((item) =>
-      item.frameworks.includes('Vue.js')
-    );
-  } else {
-    specializationStore.copySpecializationArray = specializationStore.specializationArray.slice();
-  }
-}
-
-function one() {
-  if (typescript.value === true) {
-    const typeCheckResults = specializationStore.specializationArray.filter((item) =>
-      item.frameworks.includes('Typescript')
-    );
-    specializationStore.copySpecializationArray = typeCheckResults;
-  } else {
-    specializationStore.copySpecializationArray = specializationStore.specializationArray.slice();
-  }
-}
-
-function second() {
-  if (kotlin.value === true) {
-    specializationStore.copySpecializationArray = specializationStore.specializationArray.filter((item) =>
-      item.frameworks.includes('Kotlin')
-    );
-  } else {
-    specializationStore.copySpecializationArray = specializationStore.specializationArray.slice();
-  }
-}
-watchEffect(() => {  
-  zero()
+watchEffect(() => {
+  specializationStore.vueSelected()
+  specializationStore.kotlinSelected()
+  specializationStore.typescriptSelected()
+  specializationStore.selectedHosting()
+  specializationStore.filterRating()
 })
 
-watchEffect(() => {  
-  one()
-}, { flush: 'post' })
-
-watchEffect(() => {  
-  second()
-}, { flush: 'post' })
+const advancedToogle = ref(true)
+const programmingToogle = ref(true)
+const hostingToogle = ref(true)
+const ratingToogle = ref(true)
 </script>
 
 <template>
-  <div class=" lg:w-1/5 md:mt-5 border-[1px] p-2">
-        <h1>Filter</h1>
+  <div class=" lg:w-1/5 border-[1px] p-2">
+    <div class="flex justify-between cursor-pointer" @click="advancedToogle = !advancedToogle">
+      <h1 class="text-yellow-400 font-bold">Advanced Filter</h1>
+      <Icon v-if="advancedToogle" class="w-5 h-5" icon="mdi:arrow-bottom" color="white" />
+      <Icon v-else class="w-5 h-5" icon="mdi:arrow-top" color="white" />
+    </div>
 
-        <div>
-          <h2 class="text-yellow-400">Program Languages</h2>
-          <div class="flex flex-col px-2 text-white">
-            <div class="flex items-center gap-2">
-              <input v-model="vue" class="w-4 h-4" id="vue.js" type="checkbox">
-              <label for="vue.js">Vue.js</label>
-            </div>
-            <div class="flex items-center gap-2">
-              <input v-model="typescript" class="w-4 h-4" id="typescript" type="checkbox">
-              <label for="typescript">Typescript</label>
-            </div>
-            <div class="flex items-center gap-2">
-              <input v-model="kotlin" class="w-4 h-4" id="kotlin" type="checkbox">
-              <label for="kotlin">Kotlin</label>
-            </div>
+    <div class="mt-2 ml-2" v-if="advancedToogle">
+      <div class="space-y-2">
+        <div class="flex justify-between cursor-pointer" @click="programmingToogle = !programmingToogle">
+          <h2 class="text-yellow-400">Programming languages</h2>
+          <Icon v-if="programmingToogle" class="w-5 h-5" icon="mdi:arrow-bottom" color="white" />
+          <Icon v-else class="w-5 h-5" icon="mdi:arrow-top" color="white" />
+        </div>
+        <div v-if="programmingToogle" class="flex flex-col px-2 gap-2 text-white">
+          <div class="flex items-center gap-2">
+            <input v-model="specializationStore.vue" class="w-4 h-4" id="vue.js" type="checkbox">
+            <label for="vue.js">Vue.js</label>
+          </div>
+          <div class="flex items-center gap-2">
+            <input v-model="specializationStore.typescript" class="w-4 h-4" id="typescript" type="checkbox">
+            <label for="typescript">Typescript</label>
+          </div>
+          <div class="flex items-center gap-2">
+            <input v-model="specializationStore.kotlin" class="w-4 h-4" id="kotlin" type="checkbox">
+            <label for="kotlin">Kotlin</label>
           </div>
         </div>
-
-        <div>
-          <h2 class="text-yellow-400">Hosting(Coming Soon)</h2>
-          <div class="flex flex-col px-2 text-white">
-            <div class="flex items-center gap-2">
-              <input class="w-4 h-4" id="vue.js" type="radio">
-              <label for="vue.js">Online</label>
-            </div>
-            <div class="flex items-center gap-2">
-              <input class="w-4 h-4" id="html" type="radio">
-              <label for="html">Offline</label>
-            </div>
-            <div class="flex items-center gap-2">
-              <input class="w-4 h-4" id="css" type="radio">
-              <label for="css">Coming Soon</label>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <h2 class="text-yellow-400">Rating(Coming Soon)</h2>
-          <div class="flex flex-col px-2 text-white">
-            <div class="flex items-center gap-2">
-              <input class="w-4 h-4" id="vue.js" type="radio">
-              <label for="vue.js">High rating</label>
-            </div>
-            <div class="flex items-center gap-2">
-              <input class="w-4 h-4" id="html" type="radio">
-              <label for="html">Low rating</label>
-            </div>
-          </div>
-        </div>
-
       </div>
-</template>
+
+      <div class="space-y-2 mt-2">
+        <div class="flex justify-between cursor-pointer" @click="hostingToogle = !hostingToogle">
+          <h2 class="text-yellow-400">Hosting</h2>
+          <Icon v-if="hostingToogle" class="w-5 h-5" icon="mdi:arrow-bottom" color="white" />
+          <Icon v-else class="w-5 h-5" icon="mdi:arrow-top" color="white" />
+        </div>
+        <div v-if="hostingToogle" class="flex flex-col px-2 gap-2 text-white">
+          <div class="flex items-center gap-2">
+            <input class="w-4 h-4" id="online" value="Online" type="radio" v-model="specializationStore.hostingSelected">
+            <label for="online">Online</label>
+          </div>
+          <div class="flex items-center gap-2">
+            <input class="w-4 h-4" id="offline" value="Offline" type="radio"
+              v-model="specializationStore.hostingSelected">
+            <label for="offline">Offline</label>
+          </div>
+          <div class="flex items-center gap-2">
+            <input class="w-4 h-4" id="coming soon" value="Coming Soon" type="radio"
+              v-model="specializationStore.hostingSelected">
+            <label for="coming soon">Coming Soon</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="space-y-2 mt-2">
+        <div class="flex justify-between cursor-pointer" @click="ratingToogle = !ratingToogle">
+          <h2 class="text-yellow-400">Rating</h2>
+          <Icon v-if="ratingToogle" class="w-5 h-5" icon="mdi:arrow-bottom" color="white" />
+          <Icon v-else class="w-5 h-5" icon="mdi:arrow-top" color="white" />
+        </div>
+        <div v-if="ratingToogle" class="flex flex-col px-2 gap-2 text-white">
+          <div class="flex items-center gap-2">
+            <input class="w-4 h-4" id="high" value="High rating" type="radio"
+              v-model="specializationStore.ratingSelected">
+            <label for="high">High rating</label>
+          </div>
+          <div class="flex items-center gap-2">
+            <input class="w-4 h-4" id="low" value="Low rating" type="radio" v-model="specializationStore.ratingSelected">
+            <label for="low">Low rating</label>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</div></template>
